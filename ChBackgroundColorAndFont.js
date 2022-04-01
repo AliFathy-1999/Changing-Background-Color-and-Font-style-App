52b806386c0fa6c88ee275bec9c59a8781e3a2fe
AliFathy-1999 <72993021+AliFathy-1999@users.noreply.github.com>
2022-04-01 17:11:19 +0200

    Update ChBackgroundColorAndFont.js

--- ChBackgroundColorAndFont.js
+++ ChBackgroundColorAndFont.js
@@ -9,23 +9,19 @@ mySelectff.onchange = ()=>{
     document.body.style.fontFamily= mySelectff.value;
     let mySelectffValue = window.localStorage.setItem("FontFamily",mySelectff.value);
 }
-document.querySelector(`[value='${localStorage.FontFamily}']`).selected = "FF";
 mySelectfs.onchange = ()=>{
         myDiv.style.fontSize = mySelectfs.value;
         let mySelectfsValue = window.localStorage.setItem("FontSize",mySelectfs.value);
     }
-    document.querySelector(`[value='${localStorage.FontSize}']`).selected = "FS";  
 mySelectfc.onchange = ()=>{
     myDiv.style.color = mySelectfc.value;
     let mySelectfcValue = window.localStorage.setItem("FontColor",mySelectfc.value);
 }
-document.querySelector(`[value='${localStorage.FontColor}']`).selected = "FC";  
+
 mySelectbg.onchange = ()=>{
     document.body.style.backgroundColor = mySelectbg.value;
     let mySelectbgValue = window.localStorage.setItem("BackgroundColor",mySelectbg.value);
 }
-document.querySelector(`[value='${localStorage.BackgroundColor}']`).selected = "BGC";
-
 document.body.style.fontFamily = window.localStorage.getItem("FontFamily");
 myDiv.style.fontSize = window.localStorage.getItem("FontSize");
 myDiv.style.color = window.localStorage.getItem("FontColor");
